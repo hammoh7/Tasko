f896157240099e63d1904e94c2d549b648287bf4
hammoh7 <soahammohaadkar@gmail.com>
2024-03-10 02:50:42 +0530

    API

---
+++ app/api/cards/[cardId]/route.ts
@@ -0,0 +1,36 @@
+import { database } from "@/lib/database";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { cardId: string } }
+) {
+  try {
+    const { userId, orgId } = auth();
+    if (!userId || !orgId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const card = await database.card.findUnique({
+      where: {
+        id: params.cardId,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+      include: {
+        list: {
+          select: {
+            title: true,
+          },
+        },
+      },
+    });
+    return NextResponse.json(card);
+  } catch (error) {
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
