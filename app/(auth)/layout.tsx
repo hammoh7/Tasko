44c565e1e8ad99b0a47834105c37ef1cc1799f54
hammoh7 <soahammohaadkar@gmail.com>
2024-03-10 02:52:02 +0530

    Auth Layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,17 +1,18 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import { ModalProvider } from "@/components/providers/modals";
+import { QueryProvider } from "@/components/providers/query";
 
-const AuthLayout = ({
-    children
-}: {
-    children: React.ReactNode;
-}) => {
-    return (
-        <ClerkProvider>
-            <Toaster />
-            {children}
-        </ClerkProvider>
-    )
-}
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider>
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
+    </ClerkProvider>
+  );
+};
 
 export default AuthLayout;
