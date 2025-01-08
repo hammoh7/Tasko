bf535100dd1feafdb67203f485ba9af6b66275a6
hammoh7 <soahammohaadkar@gmail.com>
2025-01-08 15:12:23 +0530

    Deployment

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -4,13 +4,9 @@ import Navbar from "./_components/(navbar)/page";
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-gradient-to-r from-blue-300 to-blue-500">
-      <nav>
-        <Navbar />
-      </nav>
+      <Navbar />
       <main className="pt-28 pb-25">{children}</main>
-      <footer>
-        <Footer />
-      </footer>
+      <Footer />
     </div>
   );
 };
